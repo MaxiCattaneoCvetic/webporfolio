3efc1c00607d24cc345d24f885d90e8111b5c724
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-03-12 11:03:23 -0300

    migrando a react

---
+++ src/components/presentation/Presentation.jsx
@@ -0,0 +1,35 @@
+import style from "./presentation.module.css";
+import { ReactTyped } from "react-typed";
+import { MdOutlineFileDownload } from "react-icons/md";
+
+function Presentation() {
+  return (
+    <section className={style.presentationContainer}>
+      <div className={style.imgPresentationContainer}>
+        <img src="../../../public/presentationLogo.png" alt="" />
+      </div>
+      <div className={style.textContainerPresentation}>
+        <h2 className={style.hola}>Hola!</h2>
+        <div className={style.holaSoy}>
+          <h2>Soy</h2>
+          <h1 className={style.titleName}>Maximiliano Cattaneo Cvetic</h1>
+        </div>
+        <ReactTyped
+          strings={["Desarollador Backend", "Licenciado en Administración"]}
+          typeSpeed={40}
+          backSpeed={40}
+          loop
+          className={style.typedText}
+        />
+        <div className={style.cvContainer}>
+          <a href="" className={style.cv}>
+            Descargar Cv{" "}
+          </a>
+          <MdOutlineFileDownload style={{ color: "white" }} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Presentation;
